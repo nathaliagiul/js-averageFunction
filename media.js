a88fe8888758fb4e758cbc6a663afc8b11ae0c8c
Nathalia Alves <nathalia.giulliane@outlook.com>
2024-03-15 13:27:10 -0300

    Update media.js

--- media.js
+++ media.js
@@ -1,21 +1,3 @@
-/*
-Nesse desafio você irá criar uma lista de **estudantes** e, cada estudante dentro dessa lista, deverá conter os seguintes dados:
-
-- nome;
-- nota da primeira prova;
-- nota da segunda prova.
-
-Depois de criada a lista:
-
-- [ ]  Crie uma **função** que irá calcular a média das notas de cada aluno;
-- [ ]  Supondo que a média, para esse concurso é **7**, verifique **se** cada aluno obteve sucesso ou não em entrar no concurso e mostre uma mensagem na tela.
-
-Mensagem sugerida:
-A média do(a) aluno(a) x é:
-Não foi dessa vez, x! Tente novamente!
-Parabéns, x! Você foi aprovado(a) no concurso!
-*/
-
 const students = [
     {
       name: "Luiz Johnson",
